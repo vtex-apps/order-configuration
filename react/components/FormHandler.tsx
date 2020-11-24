05a1ef9341fa07fdf408507f81411f1ea3fb486a
Gabriel Souza <gabrie.ssouza@gmail.com>
2020-11-24 13:45:04 -0300

    refactor(form-handler): use async await instead of mixing with promises

--- react/components/FormHandler.tsx
+++ react/components/FormHandler.tsx
@@ -1,21 +1,17 @@
-import React, { FC, useMemo, useState, useCallback } from 'react'
+import React, { FC, useCallback } from 'react'
 import {
   FormContext,
   JSONSchemaType,
-  JSONSubSchemaInfo,
-  getDataFromPointer,
   OnSubmitParameters,
 } from 'react-hook-form-jsonschema'
 import { useMutation } from 'react-apollo'
 import { GraphQLError } from 'graphql'
-
-import updateCustomSessionKeys from '../mutations/updateCustomSessionKeys.graphql'
-import setOrderFormCustomData from '../mutations/setOrderFormCustomData.graphql'
-import { FormProps } from '../typings/FormProps'
-import { parseSchemaError } from '../logic/schemaError'
-import { useSubmitReducer, SubmitContext } from '../logic/formState'
 import { useCssHandles } from 'vtex.css-handles'
 
+import { FormProps } from '../typings/FormProps'
+import { useSubmitReducer, SubmitContext } from '../logic/formState'
+
+import SetOrderFormCustomDataMutation from '../mutations/setOrderFormCustomData.graphql'
 const CSS_HANDLES = ['orderConfigFormWrapper'] as const
 
 export const FormHandler: FC<{
@@ -24,14 +20,7 @@ export const FormHandler: FC<{
   email: string | null
   onSuccessfulSubmit: () => void
 }> = props => {
-  const [updateCustomSessionKeyMutation, { error }] = useMutation(updateCustomSessionKeys)
-  const [updateOrderFormCustomData] = useMutation(setOrderFormCustomData)
-
-  const schemaErrors = useMemo(() => parseSchemaError(error), [error])
-  const [lastErrorFieldValues, setLastErrorFieldValues] = useState<
-    Record<string, string>
-  >({})
-
+  const [setOrderFormCustomData] = useMutation(SetOrderFormCustomDataMutation)
   const [submitState, dispatchSubmitAction] = useSubmitReducer()
 
   const onSubmit = useCallback(
@@ -41,57 +30,49 @@ export const FormHandler: FC<{
       }
       dispatchSubmitAction({ type: 'SET_LOADING' })
 
-      await fetch('/api/sessions/', {
-        method: 'POST',
-        body: JSON.stringify({
-      "public":{
-      "customSessionKeys":{
-      "value": JSON.stringify(data)
-      }
-      }
-        }),
-        credentials: 'same-origin',
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-        }
-      })      
-        
-      await updateOrderFormCustomData({
-            variables: {
-              appId: 'orderConfig',
-              field: 'values',
-              value: JSON.stringify(data),
-            }
-          }).then(() => {
-            dispatchSubmitAction({ type: 'SET_SUCCESS' })
-            const { onSuccessfulSubmit } = props
-            onSuccessfulSubmit()
-          })
-        
-        .catch(e => {
-          setLastErrorFieldValues(data)
-
-          if (e.graphQLErrors) {
-            for (const graphqlError of e.graphQLErrors as GraphQLError[]) {
-              if (
-                graphqlError.extensions?.exception?.name === 'UserInputError'
-              ) {
-                dispatchSubmitAction({ type: 'SET_USER_INPUT_ERROR' })
-              } else {
-                dispatchSubmitAction({
-                  type: 'SET_SERVER_INTERNAL_ERROR',
-                })
+      try {
+        await fetch('/api/sessions/', {
+          method: 'POST',
+          body: JSON.stringify({
+            "public": {
+              "customSessionKeys": {
+                "value": JSON.stringify(data)
               }
             }
-          } else {
-            dispatchSubmitAction({ type: 'SET_SERVER_INTERNAL_ERROR' })
+          }),
+          credentials: 'same-origin',
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8'
           }
-
-          methods.triggerValidation()
         })
+x
+        await setOrderFormCustomData({
+          variables: {
+            appId: 'orderConfig',
+            field: 'values',
+            value: JSON.stringify(data),
+          }
+        })
+
+        dispatchSubmitAction({ type: 'SET_SUCCESS' })
+        const { onSuccessfulSubmit } = props
+        onSuccessfulSubmit()
+      } catch (e) {
+        if (!e.graphQLErrors) return dispatchSubmitAction({ type: 'SET_SERVER_INTERNAL_ERROR' })
+
+        for (const graphqlError of e.graphQLErrors as GraphQLError[]) {
+          dispatchSubmitAction({
+            type: graphqlError.extensions?.exception?.name === 'UserInputError' ?
+              'SET_USER_INPUT_ERROR' :
+              'SET_SERVER_INTERNAL_ERROR'
+          })
+        }
+      }
+
+      methods.triggerValidation()
+
     },
     [
-      updateCustomSessionKeyMutation,
       updateOrderFormCustomData,
       dispatchSubmitAction,
     ]
@@ -103,21 +84,6 @@ export const FormHandler: FC<{
     <FormContext
       schema={props.schema}
       onSubmit={onSubmit}
-      customValidators={{
-        graphqlError: (value, context: JSONSubSchemaInfo) => {
-          const lastValue = getDataFromPointer(
-            context.pointer,
-            lastErrorFieldValues
-          )
-          if (
-            schemaErrors[context.pointer] &&
-            ((!lastValue && !value) || lastValue === value)
-          ) {
-            return schemaErrors[context.pointer][0]
-          }
-          return true
-        },
-      }}
     >
       <SubmitContext.Provider value={submitState}>
         <div className={handles.orderConfigFormWrapper}>
